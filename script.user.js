d300eaab417481489b4c19a375927ed4726a5717
Darien <darien2105@gmail.com>
2024-06-27 03:12:43 +1000

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -9,9 +9,11 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // @downloadURL  https://github.com/Multarix/force-youtube-subscription-list-view/raw/master/script.user.js
-// @updateURL     https://github.com/Multarix/force-youtube-subscription-list-View/raw/master/script.user.js
+// @updateURL    https://github.com/Multarix/force-youtube-subscription-list-View/raw/master/script.user.js
 
 // ==/UserScript==
+
+let cookieIsFine = false;
 
 function getCookie(cname) {
 	let name = cname + "=";
@@ -30,12 +32,27 @@ function getCookie(cname) {
 }
 
 
+// function getCookie(cname){
+// 	return "f6=40000400&tz=Australia.Brisbane";
+// }
+
+
 const reg = /f6\=(\d+)&(.+)/g
 const cook = getCookie("PREF");
 
 const regFoundArray = reg.exec(cook);
 let dataToChange = regFoundArray[1];
-dataToChange = dataToChange.slice(0, dataToChange.length - 1) + "1";
 
-const newData = `f6=${dataToChange}&${regFoundArray[2]}`
-document.cookie = "PREF=" + newData;
+
+if(dataToChange.endsWith("1")) cookieIsFine = true;
+console.log(cookieIsFine);
+
+
+if(!cookieIsFine){
+	dataToChange = dataToChange.slice(0, dataToChange.length - 1) + "1";
+
+	const newData = `f6=${dataToChange}&${regFoundArray[2]}`
+	document.cookie = "PREF=" + newData;
+	
+	if(window.location.href.includes("/feed/subscriptions")) location.reload();
+}
